7bb07d54f84877481a9c45ad86186576db752247
Byakoren <thomas.gouez-hoarau@live.fr>
2025-06-05 09:39:56 +0200

    Update API_BASE_URL to current backend endpoint

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 // app/config.ts
 
 // === URL de l'API back ===
-export const API_BASE_URL = 'https://b7ca-2001-41d0-fc22-6f12-d05a-7179-863-5025.ngrok-free.app'; // remplace par l'actuelle
+export const API_BASE_URL = 'https://a9a7-2001-41d0-fc22-6f12-617f-71ef-2ea7-65ac.ngrok-free.app'; // remplace par l'actuelle
 
 // Pour plus tard : versions locales/dev/prod
 export const IS_DEV = true;
