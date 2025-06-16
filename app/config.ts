1a1c582014cd0df8ec1dc24382830557fdeb481c
Byakoren <thomas.gouez-hoarau@live.fr>
2025-06-16 19:15:01 +0200

    fix: update API_BASE_URL to the current production endpoint

--- app/config.ts
+++ app/config.ts
@@ -1,13 +1,7 @@
 // app/config.ts
 
 // === URL de l'API back ===
-<<<<<<< feature/reset-password
-export const API_BASE_URL = 'https://61bc-2a01-cb00-670-8900-24d2-69f-f94a-1e32.ngrok-free.app'; // remplace par l'actuelle
-=======
-
-export const API_BASE_URL = 'https://5fa1-2a01-cb00-17a-6100-c53b-e275-d3a6-25ef.ngrok-free.app'; // remplace par l'actuelle
-
->>>>>>> main
+export const API_BASE_URL = 'http://45.80.20.27:32343'; // remplace par l'actuelle
 
 // Pour plus tard : versions locales/dev/prod
 export const IS_DEV = true;
